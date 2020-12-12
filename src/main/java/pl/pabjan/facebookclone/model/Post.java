d3ae7d314177152ea7810b75d6598704ddb438a4
Artur Pabjan <artur.pabjan00@gmail.com>
2020-12-13 00:35:13 +0100

    Created facebook-clone application

---
+++ src/main/java/pl/pabjan/facebookclone/model/Post.java
@@ -0,0 +1,4 @@
+package pl.pabjan.facebookclone.model;
+
+public class Post {
+}
---
+++ src/main/java/pl/pabjan/facebookclone/model/User.java
@@ -0,0 +1,48 @@
+package pl.pabjan.facebookclone.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(max = 50)
+    private String password;
+
+    @NotBlank
+    @Size(max = 30)
+    private String name;
+
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+
+    @NotBlank
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate birthday;
+
+    private boolean enabled;
+
+    private Instant created;
+}
